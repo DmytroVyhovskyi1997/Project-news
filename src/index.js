fb7aa5b3f04d7a3e5709373b83358da41fae37a5
DmytroVyhovskyi1997 <111079087+DmytroVyhovskyi1997@users.noreply.github.com>
2023-02-26 20:25:46 +0200

    push

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import './js/refs/refs';
+import './js/mobileMenu/mobileMenu';
+import './js/mobileMenu/mobileMenuCurrent';
+
+import './js/markup/markup';
+
+import './js/newsFilter/filter';
+import './js/newsCard/newsCard';
+import './js/api/weather';
+import './js/weather/weather';
+
+import './js/pagination/search';
+import './js/pagination/fillterPagination';
+import './js/pagination/popular';
+import './js/calendar/calendar';
+
+import './js/loader/loader';
+import './js/darkMode/darkMode';
+
+import './js/currentPage/currentPage';
